39616ff75c55677f7c9cbf4fb2cbdd06e60977fc
Montre Davis <montredavis@beck-technology.com>
2017-06-18 11:09:37 -0500

    setting up entity

--- SignalRDemo/App_Start/BundleConfig.cs
+++ SignalRDemo/App_Start/BundleConfig.cs
@@ -3,13 +3,23 @@ using System.Web.Optimization;
 
 namespace SignalRDemo
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class BundleConfig
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bundles"></param>
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/signlaR").Include(
+                         "~/Scripts/signalR-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
--- SignalRDemo/Controllers/HomeController.cs
+++ SignalRDemo/Controllers/HomeController.cs
@@ -1,18 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using SignalRDemo.Models;
+using SpotifyAPI.Web.Models;
 
 namespace SignalRDemo.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
+            using (var db = new MusicContext())
+            {
+                var savedTrack = new SavedTrack();
+
+                db.SavedTrack.Add(savedTrack);
+                db.SaveChanges();
+
+                // Display all Blogs from the database 
+                var query = db.Track.First();
+
+            }
+
             return View();
+        }
+
+        public class MusicContext : DbContext
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<SavedTrack> SavedTrack { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<AudioFeatures> AudioFeatures { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<AvailabeDevices> AvailabeDevices { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<BasicModel> BasicModel { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<Category> Category { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<CategoryPlaylist> CategoryPlaylist { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<Device> Device { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<FullAlbum> FullAlbum { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<FullTrack> FullTrack { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<FullArtist> FullArtist { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<FullPlaylist> FullPlaylist { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<SimpleArtist> SimpleArtist { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<SimpleTrack> SimpleTracks { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public DbSet<SimpleAlbum> SimpleAlbum { get; set; }
         }
     }
 }
